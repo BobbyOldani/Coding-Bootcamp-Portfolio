4843940cf2c27d1631feb385e0a66ae08b5c0473
Noisey Turtle <bobbyoldani2@gmail.com>
2020-02-01 16:53:17 -0800

    basic layout for portfolio page complete

---
+++ script.js
@@ -0,0 +1,9 @@
+//Might not have much js on this page but I wanted to do something...
+const learnBtn = $("#learnBtn");
+
+function doSomething(){
+    console.log("I did something");
+};
+
+$(learnBtn).on("click", doSomething);
+
