bd4389b06df0d09ac2853f654e6076363deea0fd
Noisey Turtle <bobbyoldani2@gmail.com>
2020-02-03 18:14:47 -0800

    site layout and general style complete

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
 //Might not have much js on this page but I wanted to do something...
 const learnBtn = $("#learnBtn");
+const quoteBtn = $("#quoteBtn");
 
-function doSomething(){
-    console.log("I did something");
+function doSomething() {
+    console.log("I did something"); 
 };
 
+function motivation() {
+    $("#quote").empty();
+    $("#author").empty();
+    $.ajax({
+        async: true,
+        crossDomain: true,
+        url: "https://type.fit/api/quotes",
+        method: "GET"
+    }).then(function(response) {
+            $()
+            let random = JSON.parse(response)[Math.floor(Math.random() * JSON.parse(response).length)];
+            let quote = random.text;
+            let author = random.author;
+            $("#quote").append(quote);
+            $("#author").append(`-${author}`);
+            console.log(random);
+        });
+}
+
+$(quoteBtn).on("click", motivation);
+$(document).ready(motivation);
 $(learnBtn).on("click", doSomething);
+
